0885781d1c4b933f0725a789e620f7ad19ccae50
hyq1860 <pchero@163.com>
2015-06-09 16:32:16 +0800

    增加require的示例

---
+++ Knockout/Controllers/RequireController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Knockout.Controllers
+{
+    public class RequireController : BaseController
+    {
+        //
+        // GET: /Require/
+        public ActionResult RequireTextPlugIn()
+        {
+            return View();
+        }
+    }
+}
---
+++ Knockout/Scripts/main.js
@@ -0,0 +1,19 @@
+﻿require.config({
+    baseUrl: "/scripts",
+    paths: {
+        "jquery": "jquery-2.1.4.min",
+        "underscore": "underscore.min",
+        "text":"text"
+    }
+
+});
+
+require(['jquery','text'], function ($,requireText) {
+
+    // some code here
+    console.log($("body").html());
+
+    require(['text!' +"/home/index"], function (content) {
+        console.log(content);
+    });
+});
