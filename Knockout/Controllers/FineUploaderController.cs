ce8208fabc759d1b9f9bd1dafcf49b4dbff08f79
hyq1860 <pchero@163.com>
2015-06-12 18:58:30 +0800

    新增fineuploader组件示例

---
+++ Knockout/Controllers/FineUploaderController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Knockout.Controllers
+{
+    public class FineUploaderController : Controller
+    {
+        //
+        // GET: /FineUploader/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult FineUploaderCore()
+        {
+            return View();
+        }
+
+    }
+}
