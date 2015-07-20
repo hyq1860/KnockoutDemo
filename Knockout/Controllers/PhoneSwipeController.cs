052111369e4052b2d358dc6e84f59842ff51f6ad
hyq1860 <pchero@163.com>
2015-07-20 21:04:10 +0800

    PhotoSwipe

---
+++ Knockout/Controllers/PhoneSwipeController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Knockout.Controllers
+{
+    public class PhoneSwipeController : BaseController
+    {
+        //
+        // GET: /PhoneSwipe/
+
+      
+        public ActionResult PhotoSwipeDemo()
+        {
+            return View();
+        }
+
+    }
+}
